7573fe09761668f6125ac010e082d27b61862b63
giovannagrigo <giovanna1grigo@gmail.com>
2026-02-02 09:12:34 -0300

    Integração com o Supabase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://yckanmvidlfmofuchwpf.supabase.co',
+    supabaseKey: 'sb_publishable_k85302JrecmBdCXEBrSmQQ_3uSL2V6w'
+};
