9a5b2767b43be618db2a2d5227147b220fc38ca9
giovannagrigo <giovanna1grigo@gmail.com>
2026-02-03 11:59:32 -0300

    Aplicando Metatags dinâmicas e Resolver

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -9,12 +9,13 @@ import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Product } from '../../interfaces/product';
 import { ProductService } from '../../services/product.service';
 import { appShellRender } from "../../directives/app-shell-render.directive";
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { appShellNoRender } from "../../directives/app-shell-no-render.directive";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-detail',
@@ -32,26 +33,36 @@ import { appShellNoRender } from "../../directives/app-shell-no-render.directive
     RouterLink,
     appShellRender,
     appShellNoRender
-],
+  ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent implements OnInit {
   quantities: number[] = [1, 2, 3, 4, 5];
-  product$!: Observable<Product | null>;
+  product!: Product;
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
-  ) {}
+    private productService: ProductService,
+    private title: Title,
+    private meta: Meta
+  ) { }
 
   ngOnInit(): void {
-    const productId = parseInt(this.route.snapshot.paramMap.get('id') ?? '0', 10);
-    this.product$ = this.productService.getProductById(productId).pipe(
-      catchError(error => {
-        console.error('Erros fetching product', error)
-        return of(null);
-      })
-    )
+    this.product = this.route.snapshot.data['product'];
+    if (this.product) {
+      this.setPageMeta(this.product)
+    }
+  }
+
+  setPageMeta(product: Product) {
+    this.title.setTitle(`${product.title} - Detalhes do produto`);
+    this.meta.addTags([
+      { name: 'description', content: product.ingredients },
+      { property: 'og:title', content: product.title },
+      { property: 'og:description', content: product.ingredients },
+      { property: 'og:image', content: product.imageDetails },
+      { name: 'twitter:card', content: 'summary_large_image' }
+    ])
   }
 }
